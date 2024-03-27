da6b4d4ce4a50b20b54c873e371968a5c1632414
ginger <26461046+gingerchew@users.noreply.github.com>
2024-03-27 19:02:02 -0400

    Adds integrations
    also installs ultrahtml for bentobox component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
-import critters from "astro-critters";
-/*
-const bootstrap = () => {
-  return {
-    name: 'bootstrap',
-    hooks: {}
-  };
-};
-*/
+// import critters from "astro-critters";
+import icon from "astro-icon";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [critters()],
+  integrations: [icon({
+    include: {
+      'pepicons-pencil': ['sun', 'moon']
+    }
+  }), purgecss()],
   site: 'https://studiolumina.github.io',
-  base: 'dad'
+  base: import.meta.env.DEV ? '' : 'dad'
 });
