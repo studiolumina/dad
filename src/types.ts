6c63d2e1e98ee900945d53206953ef4b6b550f14
ginger <26461046+gingerchew@users.noreply.github.com>
2024-03-28 09:29:13 -0400

    Adds a inferred type to get the props from the astro:assets Image

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type { Image } from "astro:assets";
+
 export type Sizes = 'sm' | 'md' | 'lg';
 export type Style = 'primary'|'secondary'|'success'|'danger'|'warning'|'info'|'light'|'dark';
 
@@ -8,5 +10,7 @@ export interface ImageProps  {
     height: `${number}`|number;
     link?: string;
 }
+
+export type AstroImageProps = Pick<Parameters<typeof Image>, 0>[0];
 
 export type Positions = 'start' | 'center' | 'end';
