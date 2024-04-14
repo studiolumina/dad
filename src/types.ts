c160f77b01fc9c00b407c00830c6ee7918fdc2a0
ginger <26461046+gingerchew@users.noreply.github.com>
2024-04-14 17:22:15 -0400

    Adds the colors from preset-mini as a type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
+// @unocss-include
 import type { Image } from "astro:assets";
+import { colors } from '@unocss/preset-mini';
 
 export type Sizes = 'sm' | 'md' | 'lg';
-export type Style = 'primary'|'secondary'|'success'|'danger'|'warning'|'info'|'light'|'dark';
+export type Styles = Lowercase<'current'|'inherit'|'transparent'|'light'|'dark'|'white'|'black'|`${keyof Omit<typeof colors, 'current'|'inherit'|'transparent'|'light'|'dark'|'white'|'black'>}-${50|100|200|300|400|500|600|700|800|900|950}`>;
 export type Themes = 'light' | 'dark';
 
 export interface ImageProps  {
